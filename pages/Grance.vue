74fbe732955ac72a76129cc5393b81ec277bac77
yuta4j1 <kascado.ys10@gmail.com>
2019-07-12 23:54:26 +0900

    fetch posts from vuex

--- pages/Grance.vue
+++ pages/Grance.vue
@@ -7,60 +7,20 @@
 </template>
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator'
+import { vxm } from '../store/store.vuex'
 import { Post } from '../models/Post'
-import PostPanel from '../components/PostPanel.vue';
+import PostPanel from '../components/PostPanel.vue'
 
 @Component({
     components: { PostPanel }
 })
 export default class Grance extends Vue {
-    posts: Post[] = [
-        {
-            id: '1',
-            title: 'test1',
-            content: '',
-            ownerId: 'foo',
-            remarks: 'AAAAAAAAAA',
-            insertDateTime: '2019/06/27 00:00:00',
-            updateDateTime: '2019/06/27 00:00:00'
-        },
-        {
-            id: '2',
-            title: 'test2',
-            content: '',
-            ownerId: 'bbbbbbbbbbbbbb',
-            remarks: 'testtest',
-            insertDateTime: '2019/06/27 00:00:00',
-            updateDateTime: '2019/06/27 00:00:00'
-        },
-        {
-            id: '3',
-            title: 'test3',
-            content: '',
-            ownerId: 'hoge',
-            remarks: 'CCCCCCCCCCCCCCC',
-            insertDateTime: '2019/06/27 00:00:00',
-            updateDateTime: '2019/06/27 00:00:00'
-        },
-        {
-            id: '4',
-            title: 'test4',
-            content: '',
-            ownerId: 'fuga',
-            remarks: 'dddddddddd',
-            insertDateTime: '2019/06/27 00:00:00',
-            updateDateTime: '2019/06/27 00:00:00'
-        },
-        {
-            id: '5',
-            title: 'test5',
-            content: '',
-            ownerId: 'yyyy',
-            remarks: 'EEEEEEEEEEE',
-            insertDateTime: '2019/06/27 00:00:00',
-            updateDateTime: '2019/06/27 00:00:00'
-        }
-    ]
+    posts: Post[] = []
+    mounted() {
+        vxm.post.fetchPosts().then((data: Post[]) => {
+            this.posts = data
+        })
+    }
 }
 </script>
 
