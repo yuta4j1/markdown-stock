111b78ee2b710b1f3dde3c19ca92ad90e507a79a
yuta4j1 <kascado.ys10@gmail.com>
2019-07-12 23:56:28 +0900

    feat: 'post', 'editor' store

--- store/store.vuex.ts
+++ store/store.vuex.ts
@@ -1,12 +1,18 @@
 import Vuex from 'vuex'
-import { UserStore } from './user'
+import UserStore from './user'
+import PostStore from './post'
+import EditorStore from './editor'
 
 export const store = new Vuex.Store({
     modules: {
         user: UserStore.ExtractVuexModule(UserStore),
+        post: PostStore.ExtractVuexModule(PostStore),
+        editor: EditorStore.ExtractVuexModule(EditorStore)
     }
 })
 
 export const vxm = {
-    user: UserStore.CreateProxy(store, UserStore)
+    user: UserStore.CreateProxy(store, UserStore),
+    post: PostStore.CreateProxy(store, PostStore),
+    editor: EditorStore.CreateProxy(store, EditorStore)
 }
