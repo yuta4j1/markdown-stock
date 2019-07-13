e0cd93f2fa5b9a1b90691f42552ed53a8e8843d6
yuta4j1 <kascado.ys10@gmail.com>
2019-07-14 02:55:42 +0900

    add PostPanelHeader

---
+++ components/PostPanelHeader.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="pp__container">
+      <div class="long-part"></div>
+      <div class="long-part">タイトル</div>
+      <div class="long-part">カテゴリ</div>
+      <div class="part">コメント</div>
+      <div class="long-part">投稿日時</div>
+      <div class="long-part">更新日時</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class PostPanelHeader extends Vue {
+
+} 
+</script>
+
+<style scoped>
+.pp__container {
+    display: flex;
+    justify-content: center;
+    padding: 8px;
+    border-bottom-style: solid;
+    border-width: 0.5px;
+    border-color: #E0E0E0;
+}
+
+.part {
+    margin: 0px 20px 0px 20px; 
+    width: 100px;
+}
+
+.long-part {
+    margin: 0px 20px 0px 20px; 
+    width: 200px;
+}
+
+</style>
