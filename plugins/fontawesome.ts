0926682f43e473f802c905a751996a1668455bac
yuta4j1 <kascado.ys10@gmail.com>
2019-07-23 23:58:40 +0900

    add icon

--- plugins/fontawesome.ts
+++ plugins/fontawesome.ts
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faPen } from '@fortawesome/free-solid-svg-icons'
+import { faPen, faEllipsisH } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 library.add(faPen)
+library.add(faEllipsisH)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
