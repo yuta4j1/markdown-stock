c9a6290eef9e17b8f7655c4f53cd88c32ad633f8
yuta4j1 <kascado.ys10@gmail.com>
2019-07-24 21:27:29 +0900

    fix: delete pen icon

--- plugins/fontawesome.ts
+++ plugins/fontawesome.ts
@@ -1,9 +1,8 @@
 import Vue from 'vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faPen, faEllipsisH } from '@fortawesome/free-solid-svg-icons'
+import { faEllipsisH } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faPen)
 library.add(faEllipsisH)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
