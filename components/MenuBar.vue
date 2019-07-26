834968161709a2879b695cd9be4d35b0c74e5b13
yuta4j1 <kascado.ys10@gmail.com>
2019-07-26 23:57:21 +0900

    'MenuBar' component

---
+++ components/MenuBar.vue
@@ -0,0 +1,37 @@
+<template>
+<div class="menu-wrapper">
+    <div class="menu-area">
+        <div class="menu-item"><p>Markdown Stock</p></div>
+    </div>
+</div>
+    
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component
+export default class MenuBar extends Vue {
+
+}
+</script>
+
+<style scoped>
+.menu-wrapper {
+    position: fixed;
+    width: 100vw;
+    height: 7%;
+    display: flex;
+    justify-content: center;
+    border-bottom-style: solid;
+    border-bottom-width: 2.0px;
+    border-bottom-color: #EEEEEE;
+}
+.menu-area {
+    /* position: fixed; */
+    width: 64%;
+    display: flex;
+    justify-content: left;
+}
+
+</style>
