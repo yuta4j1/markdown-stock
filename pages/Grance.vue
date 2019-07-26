e782c64a2809f9e8ca15e547b8197dcf294f1e67
yuta4j1 <kascado.ys10@gmail.com>
2019-07-26 23:58:15 +0900

    feat: Menubar

--- pages/Grance.vue
+++ pages/Grance.vue
@@ -1,14 +1,22 @@
 <template>
-  <div class="p__container">
-    <div class="p__table">
-      <PostPanelHeader />
-      <div v-for="(post, idx) in posts" :key="idx">
-        <PostPanel :post="post" :showModal="showDeleteModal" />
-      </div>
+  <div class="grance">
+    <MenuBar />
+    <div class="p__container">
+        <div class="p__table">
+        <PostPanelHeader />
+        <div v-for="(post, idx) in posts" :key="idx">
+            <PostPanel :post="post" :showModal="showDeleteModal" />
+        </div>
+        </div>
+        <Modal :isOpen="dialogOpen">
+            <DialogMessage :msgProps="deleteConfirmMsg" :onOk="doDelete" :onClose="modalClose" />
+        </Modal>
     </div>
-    <Modal :isOpen="dialogOpen">
-        <DialogMessage :msgProps="deleteConfirmMsg" :onOk="doDelete" :onClose="modalClose" />
-    </Modal>
+    <div>
+    <nuxt-link class="to-editor-btn" to="/editor">
+        <font-awesome-icon icon="edit" size="2x" />
+    </nuxt-link>
+    </div>
   </div>
 </template>
 <script lang="ts">
@@ -16,14 +24,15 @@ import { Vue, Component } from 'vue-property-decorator'
 import { vxm } from '../store/store.vuex'
 import db from '../plugins/firestore'
 import { Post } from '../models/Post'
-import { Message, DialogMessageProps } from '../models/Message';
+import { Message, DialogMessageProps } from '../models/Message'
+import MenuBar from '../components/MenuBar.vue'
 import PostPanel from '../components/PostPanel.vue'
 import PostPanelHeader from '../components/PostPanelHeader.vue'
 import Modal from '../components/Modal.vue'
-import DialogMessage from '../components/DialogMessage.vue';
+import DialogMessage from '../components/DialogMessage.vue'
 
 @Component({
-    components: { PostPanelHeader, PostPanel, Modal, DialogMessage }
+    components: { PostPanelHeader, PostPanel, Modal, DialogMessage, MenuBar }
 })
 export default class Grance extends Vue {
     posts: Post[] = []
@@ -35,6 +44,7 @@ export default class Grance extends Vue {
           type: 'confirm',
           message: "テキストを削除します。よろしいですか？"
         },
+        okIsLink: false,
         onOk: this.doDelete,
         onCancel: this.modalClose
     }
@@ -75,9 +85,25 @@ export default class Grance extends Vue {
     display: flex;
     justify-content: center;
     margin: 0;
-    padding-top: 40px;
+    padding-top: 90px;
     padding-right: 0px;
     width: 95%;
+}
+
+.to-editor-btn {
+    /* position: fixed; */
+    top: 10%;
+    left: 92%;
+    text-decoration: none;
+    border-radius: 50%;
+    width: 60px;
+    height: 60px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #212121;
+    box-shadow: 10px 10px 10px rgba(0,0,0,0.4);
+
 }
 
 </style>
