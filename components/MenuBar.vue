978d84316001805508eef90ea108573ca397e140
yuta4j1 <kascado.ys10@gmail.com>
2019-08-04 23:57:46 +0900

    feat: create new button

--- components/MenuBar.vue
+++ components/MenuBar.vue
@@ -1,7 +1,13 @@
 <template>
 <div class="menu-wrapper">
     <div class="menu-area">
-        <div class="menu-item"><p>Markdown Stock</p></div>
+        <div class="menu-main"><p>Markdown Stock</p></div>
+    </div>
+    <div class="menu-item">
+        <nuxt-link class="edit-btn" to="/editor">
+            新規作成 
+            <font-awesome-icon icon="edit" />
+        </nuxt-link>
     </div>
 </div>
     
@@ -17,15 +23,23 @@ export default class MenuBar extends Vue {
 </script>
 
 <style scoped>
+.menu-main {
+    font-size: 1.2rem;
+}
+
 .menu-wrapper {
     position: fixed;
     width: 100vw;
     height: 7%;
     display: flex;
     justify-content: center;
+    align-items: center;
     border-bottom-style: solid;
     border-bottom-width: 2.0px;
     border-bottom-color: #EEEEEE;
+    opacity: 0.8;
+    background-color: #FFFFFF;
+    z-index: 2;
 }
 .menu-area {
     /* position: fixed; */
@@ -33,5 +47,31 @@ export default class MenuBar extends Vue {
     display: flex;
     justify-content: left;
 }
+
+.edit-btn {
+    text-decoration: none;
+    padding: 8px;
+    border-radius: 5px;
+    background-color: #F5F5F5;
+    color: #212121;
+}
+
+@keyframes mouseon-edit {
+    0% {
+        background-color: #F5F5F5;
+    }
+    60% {
+        background-color: #EEEEEE;
+    }
+    100% {
+        background-color: #E0E0E0;
+    }
+}
+
+.edit-btn:hover {
+    animation: mouseon-edit 0.1s forwards;
+}
+
+
 
 </style>
