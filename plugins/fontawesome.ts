9ad6f8f6d75cb0f206161597552e13b8f539988f
yuta4j1 <kascado.ys10@gmail.com>
2019-11-13 21:34:56 +0900

    add icon

--- plugins/fontawesome.ts
+++ plugins/fontawesome.ts
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faEdit, faEllipsisH } from '@fortawesome/free-solid-svg-icons'
+import { faEdit, faEllipsisH, faLayerGroup } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faEdit, faEllipsisH)
+library.add(faEdit, faEllipsisH, faLayerGroup)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
