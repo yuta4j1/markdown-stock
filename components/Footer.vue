915d3481484e72be471dad4dbd0d9f2694629b20
yuta4j1 <kascado.ys10@gmail.com>
2019-11-13 21:35:23 +0900

    Footer component

---
+++ components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="footer"></div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component
+export default class Footer extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+.footer {
+  width: 100vw;
+  height: 24px;
+}
+</style>
