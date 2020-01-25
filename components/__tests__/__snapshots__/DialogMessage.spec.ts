f9e03574fddac6d8541d718f9ce55e3b9a0ddb99
yuta4j1 <kascado.ys10@gmail.com>
2020-01-25 13:48:50 +0900

    update test

--- components/__tests__/__snapshots__/DialogMessage.spec.ts
+++ components/__tests__/__snapshots__/DialogMessage.spec.ts
@@ -1,10 +1,20 @@
 import { mount } from '@vue/test-utils'
-import DialogMessage from '@components/DialogMessage.vue'
+import DialogMessage from '../../DialogMessage.vue'
+import { DialogMessageProps } from '../../../models/Message'
 
 describe('DialogMessage component', () => {
     test('snapshot', () => {
         const wrapper = mount(DialogMessage)
-        wrapper.setProps({ message: 'This is a Message test.' })
+        const props: DialogMessageProps = {
+            message: {
+                type: 'success',
+                message: 'Message test.'
+            },
+            okIsLink: false,
+            onOk: () => {},
+            onCancel: () => {}
+        }
+        wrapper.setProps(props)
         expect(wrapper.element).toMatchSnapshot()
     })
 
